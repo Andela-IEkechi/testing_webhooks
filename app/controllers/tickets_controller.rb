b96f0635217aa6aa1e57080a8dbdc4060cc785be
elarex <jeanlrx@gmail.com>
2012-07-23 11:30:57 -0700

    workign on crud screens for ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,4 +2,35 @@ class TicketsController < ApplicationController
 	def index
 		@tickets = Ticket.all
 	end
+
+  def show
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def new
+    @ticket = Ticket.new()
+  end
+
+  def create
+    @ticket = Ticket.build(params[:ticket])
+    if @ticket.save
+      flash[:info] = "Ticket was added"
+      redirect_to ticket_path(@ticket)
+    else
+      flash[:alert] = "Ticket could not be created"
+      render :template => 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
