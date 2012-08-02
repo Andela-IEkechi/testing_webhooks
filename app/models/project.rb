7ff9ad90cf9f2c56a57115bc0145205b564bb88c
elarex <jeanlrx@gmail.com>
2012-08-02 08:47:10 -0700

    add nested form

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,8 @@ class Project < ActiveRecord::Base
   has_many :tickets, :as => :ticketable
   has_many :ticket_statuses
 
-  attr_accessible :title
+  attr_accessible :title, :ticket_statuses_attributes
+  accepts_nested_attributes_for :ticket_statuses
 
   validates :title, :presence => true
 
