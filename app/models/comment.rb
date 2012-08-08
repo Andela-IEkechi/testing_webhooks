aac6bfe82e7329651e26bfe55cc8ac3ac34e2b6b
elarex <jeanlrx@gmail.com>
2012-08-08 02:44:47 -0700

    allow tickets to be assigned to features after the fact

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,8 +4,27 @@ class Comment < ActiveRecord::Base
 
   attr_accessible :body, :ticket_id, :status_id, :status
 
+  #we use these to the ticket values for sprint and feature from a new comment
+  attr_accessor :feature_id, :sprint_id
+  attr_accessible :feature_id, :sprint_id
+  after_create :set_ticket_values
+
   validates :ticket_id, :presence => true
 
   scope :with_status, lambda { {:conditions => ["status_id IS NOT NULL"]} }
 
+  def feature_id
+    ticket.feature_id
+  end
+
+  def sprint_id
+    ticket.sprint_id
+  end
+
+  private
+  def set_ticket_values
+    self.ticket.feature_id = feature_id
+    self.ticket.sprint_id = sprint_id
+    self.ticket.save!
+  end
 end
