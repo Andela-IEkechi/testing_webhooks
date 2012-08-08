31e97459d3436269c831248a8b9eb3584adeedc2
elarex <jeanlrx@gmail.com>
2012-08-07 23:47:48 -0700

    add helpers for figuring out the ticket paths

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,10 +1,27 @@
 module TicketsHelper
 
-  def parent_path(ticket)
-    return project_path(ticket.project) if ticket.project
-    return project_feature_path(ticket.project, ticket.feature) if ticket.feature
-    tickets_path()
+  def ticket_parent_path(ticket)
+    if ticket.feature
+      project_feature_path(ticket.project, ticket.feature)
+    else
+      project_path(ticket.project)
+    end
   end
 
+  def ticket_edit_path(ticket)
+    if ticket.feature
+      project_feature_path(@ticket.project, @ticket.feature)
+    else
+      project_path(@ticket.project)
+    end
+  end
+
+  def ticket_new_path(ticket)
+    if ticket.feature
+      new_project_feature_path(@ticket.project, @ticket.feature)
+    else
+      new_project_path(@ticket.project)
+    end
+  end
 
 end
