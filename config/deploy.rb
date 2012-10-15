cb1093ea47fb403ef0d6a83a64cefd527068edce
elarex <jeanlrx@gmail.com>
2012-10-15 20:50:47 +0200

    add asset compilation

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,6 @@
+require 'bundler/capistrano'
 require 'capistrano/ext/multistage'
+load 'deploy/assets'
 
 ##STAGING DEPLOYMENT IS TO HEROKU!!
 set :stages, %w(production)
@@ -16,7 +18,7 @@ set :branch, "master"
 set :deploy_to, "/home/deploy/rails/#{application}"
 set :deploy_via, :remote_cache
 set :rails_env, 'production'
-ssh_options[:forward_agent] = true
+#ssh_options[:forward_agent] = true
 
 
 namespace :deploy do
