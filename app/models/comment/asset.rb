96ae1a538bc3f2b6ec38f1ea1aff776e46017e63
elarex <jeanlrx@gmail.com>
2012-10-23 12:11:29 +0200

    add assets and uploaders specs

--- app/models/comment/asset.rb
+++ app/models/comment/asset.rb
@@ -2,6 +2,7 @@ class Comment::Asset < ActiveRecord::Base
   belongs_to :comment
   has_one :ticket, :through => :comment
 
+  mount_uploader :asset, FileUploader
   # attr_accessible :title, :body
 
   validates :comment_id, :presence => true
--- db/migrate/20121023084048_create_comment_assets.rb
+++ db/migrate/20121023084048_create_comment_assets.rb
@@ -2,6 +2,7 @@ class CreateCommentAssets < ActiveRecord::Migration
   def change
     create_table :comment_assets do |t|
       t.references :comment, :null => false
+      t.string :file
       t.timestamps
     end
   end
