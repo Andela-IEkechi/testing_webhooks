c7da69bfe6e51aa1784f80ffb72e065ef4e2bb2f
rowan <your.rowan@shuntyard.co.za>
2012-10-26 16:24:28 +0200

    get file uploads uploading

--- app/models/comment/asset.rb
+++ app/models/comment/asset.rb
@@ -1,9 +1,6 @@
 class Comment::Asset < ActiveRecord::Base
-  belongs_to :comment
-  has_one :ticket, :through => :comment
-
-  mount_uploader :asset, FileUploader
-  # attr_accessible :title, :body
-
-  validates :comment_id, :presence => true
+  belongs_to      :comment
+  has_one         :ticket, :through => :comment
+  mount_uploader  :file, FileUploader
+  attr_accessible :file
 end
