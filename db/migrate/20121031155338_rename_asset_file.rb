e6b199299471042265d65aecd3804e3886dcfa67
elarex <jeanlrx@gmail.com>
2012-10-31 18:04:31 +0200

    remove file, not needed

--- db/migrate/20121031155338_rename_asset_file.rb
+++
@@ -1,9 +0,0 @@
-class RenameAssetFile < ActiveRecord::Migration
-  def up
-    rename_column :comment_assets, :file, :payload
-  end
-
-  def down
-    rename_column :comment_assets, :payload, :file
-  end
-end
