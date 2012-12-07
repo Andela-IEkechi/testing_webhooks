fdfb1ea9ad3400e4697679294263fb4c8ba1c011
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2012-12-06 23:37:32 -0300

    Made memberships and participants accessible

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,15 +8,12 @@ class Project < ActiveRecord::Base
   has_many :tickets, :dependent => :destroy
   has_many :sprints, :order => :due_on, :dependent => :destroy
   has_many :ticket_statuses, :dependent => :destroy
-  # has_and_belongs_to_many :participants, :association_foreign_key => 'user_id', :class_name => 'User', :order => 'email asc'
 
   has_many :memberships # works
   has_many :participants, :through => :memberships, :source => :user, :order => 'email asc'
 
-  # attr_accessible :title, :ticket_statuses_attributes, :user_id, :sprint_duration, :api_key, :participant_ids, :participants_attributes
-  # accepts_nested_attributes_for :ticket_statuses, :participants
-  attr_accessible :title, :ticket_statuses_attributes, :user_id, :sprint_duration, :api_key
-  accepts_nested_attributes_for :ticket_statuses
+  attr_accessible :title, :ticket_statuses_attributes, :user_id, :sprint_duration, :api_key, :participant_ids, :participants_attributes, :memberships
+  accepts_nested_attributes_for :ticket_statuses, :memberships, :participants
 
   validates :title, :presence => true, :uniqueness => {:scope => :user_id}
   validates :api_key, :presence => true, :on => :update
