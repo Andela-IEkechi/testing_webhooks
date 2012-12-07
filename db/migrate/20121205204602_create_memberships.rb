0699687c0f1ed9f7267e2e9d30dfed250879db4a
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2012-12-06 23:37:50 -0300

    Fixed migration

--- db/migrate/20121205204602_create_memberships.rb
+++ db/migrate/20121205204602_create_memberships.rb
@@ -1,6 +1,22 @@
 class CreateMemberships < ActiveRecord::Migration
-  def change
-    rename_table 'projects_users', 'memberships'
-    add_column :memberships, :is_admin, :boolean, :null => false, :default => false
+
+  def up
+    create_table :memberships do |t|
+      t.references :project
+      t.references :user
+      t.boolean :is_admin, :null => false, :default => false
+    end
+
+    # Use the existing data from project_users to populate memberships
+    execute <<-SQL
+      INSERT INTO memberships (project_id, user_id, is_admin)
+      SELECT project_id, user_id, true FROM projects_users
+    SQL
+
   end
+
+  def down
+    drop_table :memberships
+  end
+
 end
