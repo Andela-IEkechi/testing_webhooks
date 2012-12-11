b99a08fdccf3d258496dbc2146a00da1dd52aae5
fmarais <fm.marais@gmail.com>
2012-12-11 15:28:45 +0200

    change 0 cost to read 'unkown' [#239]

--- db/migrate/20121211070217_change_cost_format_in_comments.rb
+++
@@ -1,9 +0,0 @@
-class ChangeCostFormatInComments < ActiveRecord::Migration
-  def self.up
-   change_column :comments, :cost, :string
-  end
-
-  def self.down
-   change_column :comments, :cost, :integer
-  end
-end
