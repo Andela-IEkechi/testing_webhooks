75fce8f65f82478d664c524908bf267341fce4ee
elarex <jeanlrx@gmail.com>
2012-12-17 10:16:40 +0200

    refactor to clean up the code [#207]

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -7,4 +7,20 @@ module CommentsHelper
       cost.to_s
     end
   end
+
+  def change_set(comment)
+    changeset = {:user => comment.user.to_s}
+    previous = comment.previous
+    #get what changed
+    [:status_id, :feature_id, :sprint_id, :assignee_id, :cost, :assets].each do |attr|
+      now = comment.send(attr)
+      if previous
+        was = previous.send(attr)
+        changeset[attr] = [now] if now != was
+      else
+        changeset[attr] = [now]
+      end
+    end
+    changeset
+  end
 end
