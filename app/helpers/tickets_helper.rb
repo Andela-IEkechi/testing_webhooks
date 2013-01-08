a3bc1108f75addfdc34bc937e4118b322b74c7c9
elarex <jeanlrx@gmail.com>
2013-01-08 18:36:58 +0200

    remove unused context from ticket helper

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -8,17 +8,5 @@ module TicketsHelper
     end
   end
 
-  def context_type
-    return @sprint.class.name if @sprint
-    return @feature.class.name if @feature
-    return @project.class.name if @project
-    nil
-  end
 
-  def context_id
-    return @sprint.id if @sprint
-    return @feature.id if @feature
-    return @project.id if @project
-    nil
-  end
 end
