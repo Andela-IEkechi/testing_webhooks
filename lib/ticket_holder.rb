2f801e5dd289363a5fb2f41d675027b065ab48d8
elarex <jeanlrx@gmail.com>
2013-01-19 19:22:56 +0200

    added in meter version of the progress bar [#665]

--- lib/ticket_holder.rb
+++ lib/ticket_holder.rb
@@ -21,5 +21,28 @@ module TicketHolder
     def assigned_tickets
       eval("Ticket.for_#{self.class.name.downcase}_id(#{self.id})")
     end
+
+    def open_tickets
+      assigned_tickets.collect(&:status).select{|s| s.open}
+    end
+
+    def closed_tickets
+      assigned_tickets.collect(&:status).select{|s| !s.open}
+    end
+
+    def has_open_tickets?
+      open_tickets.count > 0
+    end
+
+    def completion_percentage
+      return 0 if assigned_tickets.count == 0
+      (closed_tickets.count/(assigned_tickets.count*1.0)).round(2)
+    end
+
+    def completion_message
+      return "nothing assigned" if assigned_tickets.count == 0
+      "#{closed_tickets.count} of #{assigned_tickets.count} tickets closed"
+    end
+
   end
 end
