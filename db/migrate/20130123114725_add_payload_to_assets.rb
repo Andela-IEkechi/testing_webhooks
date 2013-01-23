5acc6d6b1fc7281579d5ed61bf7a42f20708b30b
elarex <jeanlrx@gmail.com>
2013-01-23 21:43:00 +0200

    fix migration class name

--- db/migrate/20130123114725_add_payload_to_assets.rb
+++ db/migrate/20130123114725_add_payload_to_assets.rb
@@ -1,6 +1,6 @@
 class AddPayloadToAssets < ActiveRecord::Migration
   def change
-    Comment::Assets.reset_column_information
-    add_column(:comment_assets, :payload, :string) unless Comment::Assets..column_names.include?('profile')
+    Comment::Asset.reset_column_information
+    add_column(:comment_assets, :payload, :string) unless Comment::Asset.column_names.include?('profile')
   end
 end
