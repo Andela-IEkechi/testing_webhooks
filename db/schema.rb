e27e3a1de670d4680a560d5bc493b465eb8dee35
elarex <jeanlrx@gmail.com>
2013-01-25 16:26:07 +0200

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130125103633) do
+ActiveRecord::Schema.define(:version => 20130125142145) do
 
   create_table "api_keys", :id => false, :force => true do |t|
     t.string   "name",       :null => false
