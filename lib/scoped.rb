b32cc10bcbf2ff2330e369ece8919b8bc14ac678
elarex <jeanlrx@gmail.com>
2013-01-31 12:59:15 +0200

    refactor scoped methods into a lib, include into tickets, sprints, features [#717]

---
+++ lib/scoped.rb
@@ -0,0 +1,20 @@
+module Scoped
+  extend ActiveSupport::Concern
+
+  included do
+    before_create :populate_scoped_id
+
+    def to_param
+      self.scoped_id
+    end
+
+    private
+    def populate_scoped_id
+      sequence = "#{self.class.name.downcase.pluralize}_sequence".to_sym #eg. tickets_sequence or sprints_sequence etc
+      project.increment!(sequence)
+      self.scoped_id = project.send(sequence)
+    end
+
+  end
+end
+
