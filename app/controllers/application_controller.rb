475f13db289fa424ab7566b22d3f068d7af4d962
Angus Miller <all4miller@gmail.com>
2013-02-03 15:28:41 +0200

    update layuouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def layout_by_resource
-    if devise_controller? && action_name == 'new'
+    if devise_controller? && action_name != 'edit'
       "landing"
     else
       "application"  
