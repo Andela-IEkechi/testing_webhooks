cfbbea2f3e4e82c58ea9c6536705ff260bd05c2c
fmarais <fm.marais@gmail.com>
2013-03-06 09:29:30 +0200

    added edit view and updated accounts index view [#753]

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,16 @@
 class AccountsController < ApplicationController
   def edit
-    @account = Account.find(params[:id])    
+    @account = Account.find(params[:id])
+  end
+
+  def update
+    @account = Account.find(params[:id])
+
+    if @account.update_attributes(params[:account])
+      flash[:notice] = "Your account was updated"
+    else
+      flash[:alert] = "Your account could not be updated"
+    end
+    redirect_to edit_user_account_path(@account.user,@account)
   end
 end
