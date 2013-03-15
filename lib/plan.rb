84e81be9ff9c614b71c3d400aadc7f3a133d1752
elarex <jeanlrx@gmail.com>
2013-03-15 14:07:09 +0200

    split account functions into plan module [#753]

---
+++ lib/plan.rb
@@ -0,0 +1,49 @@
+module Plan
+
+  PLANS = {
+    :free   => {:title => 'Free',               :price => 0,  :users => 3,  :projects => 1,  :storage_gb => 1},
+    :small  => {:title => 'Startups',           :price => 10, :users => 10, :projects => 5,  :storage_gb => 2},
+    :medium => {:title => 'Small Company',      :price => 25, :users => 50, :projects => 15, :storage_gb => 10, :preferred => true},
+    :large  => {:title => 'Large Organization', :price => 70, :users => 999,:projects => 100, :storage_gb => 50} #unlimited users = 999
+  }
+
+  def initialize(name)
+    @name = name
+  end
+
+  def <=>(other)
+    plan_names = PLAN.keys.collect(&:to_str)
+    plan.names.index(self) <=> plan_names.index(other)
+  end
+
+  def to_s
+    @name.to_s
+  end
+
+  def upgrade
+    new case self.name
+      when 'free'
+        return 'small'
+      when 'small'
+        return 'medium'
+      when 'medium'
+        return 'large'
+      else
+        return 'large'
+      end
+  end
+
+  def downgrade
+    new case self.name
+      when 'large'
+        return 'medium'
+      when 'medium'
+        return 'small'
+      when 'small'
+        return 'free'
+      else
+        return 'free'
+      end
+  end
+
+end
