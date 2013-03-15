9f1830c0f95064a495772ea91e520375c3c7e672
elarex <jeanlrx@gmail.com>
2013-03-15 16:12:42 +0200

    updated specs [#753]

--- spec/libs/plan.rb
+++ spec/libs/plan.rb
@@ -1,13 +1,19 @@
 require 'spec_helper'
 
-describe Participant do
+describe Participant, :focus => true do
 
+  it "define PLANS as a hash of plan types" do
+    Plan::PLANS.should_not be_nil
+    Plan::PLANS.try(:keys).should_not be_nil
+  end
 
-  it "define PLANS as a hash of plan types"
+  it "should be able to contruct from an input string" do
+    defined?(Plan.new('free')).should_not be_nil
+  end
 
-  it "should be able to contruct from an input string"
-
-  it "should construct a 'free' plan if the input is bad"
+  it "should construct a 'free' plan if the input is bad" do
+    Plan.new('bad argument').to_s.should eq('free')
+  end
 
   it "should respond to :to_s with the plan name"
 
