b5c8f6ed566e59671cd63cee14578c42521d21f9
Rowan Smith <rowan@shuntyard.co.za>
2013-03-25 12:01:00 +0200

    Membership model test spec [#354]

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
-describe Membership do
+describe Membership, :focus => true do
+  before(:each) do
+    @membership = create(:membership)
+  end
 
-  it 'should'
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should have a user' do
+    @membership.user.should_not be_nil
+  end
+
+  it 'should have a project' do
+    @membership.project.should_not be_nil
+  end
+  
+
 end
