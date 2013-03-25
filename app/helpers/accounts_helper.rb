65d10b41b86d416c3a05d84ebd52fb95b265f710
Rowan Smith <rowan@shuntyard.co.za>
2013-03-25 13:51:41 +0200

    Disable button for users current plan [#795]

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,6 +1,15 @@
 module AccountsHelper
 
     def link_text(account, plan)
-      Plan.new(account.plan).better_than?(plan) ? "downgrade" : "upgrade"
+      if account.plan == plan.to_s
+        "your plan"
+      else
+        Plan.new(account.plan).better_than?(plan) ? "downgrade" : "upgrade"
+      end
     end
+
+    def disabled?(plan)
+      return true if current_user.account.plan == plan.to_s
+    end
+
 end
