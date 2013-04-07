40772eb26c5f65a0ff56c62f92668ad2e10dd06d
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2013-04-07 16:42:14 -0400

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130222153534) do
+ActiveRecord::Schema.define(:version => 20130307011400) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "user_id"
@@ -146,6 +146,7 @@ ActiveRecord::Schema.define(:version => 20130222153534) do
     t.integer  "invitation_limit"
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
+    t.datetime "deleted_at"
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
