e24a9677d136218d2360827621f74e9ed76a7046
elarex <jeanlrx@gmail.com>
2013-04-10 13:32:44 +0200

    fix new project link [#354]

--- db/schema.rb
+++ db/schema.rb
@@ -33,10 +33,9 @@ ActiveRecord::Schema.define(:version => 20130222153534) do
 
   create_table "comment_assets", :force => true do |t|
     t.integer  "comment_id", :null => false
-    t.string   "file"
+    t.string   "payload"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.string   "payload"
   end
 
   create_table "comments", :force => true do |t|
@@ -73,7 +72,7 @@ ActiveRecord::Schema.define(:version => 20130222153534) do
   create_table "memberships", :force => true do |t|
     t.integer "project_id"
     t.integer "user_id"
-    t.string  "role",       :default => "Regular", :null => false
+    t.string  "role",       :default => "regular", :null => false
   end
 
   create_table "projects", :force => true do |t|
@@ -85,11 +84,6 @@ ActiveRecord::Schema.define(:version => 20130222153534) do
     t.integer  "features_sequence", :default => 0
     t.integer  "sprints_sequence",  :default => 0
     t.boolean  "private",           :default => true
-  end
-
-  create_table "projects_users", :id => false, :force => true do |t|
-    t.integer "project_id"
-    t.integer "user_id"
   end
 
   create_table "sprints", :force => true do |t|
@@ -143,7 +137,6 @@ ActiveRecord::Schema.define(:version => 20130222153534) do
     t.string   "uid"
     t.string   "full_name"
     t.boolean  "terms",                                :default => false
-    t.text     "preferences"
     t.string   "invitation_token",       :limit => 60
     t.datetime "invitation_sent_at"
     t.datetime "invitation_accepted_at"
