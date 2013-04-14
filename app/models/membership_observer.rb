9b577ed338f47c5b1467e527c57bdaf380652c62
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2013-04-14 18:26:27 -0400

    Project participant emails [#793]

--- app/models/membership_observer.rb
+++ app/models/membership_observer.rb
@@ -3,7 +3,10 @@ class MembershipObserver < ActiveRecord::Observer
 
   cattr_accessor :disable_notifications # use this when doing migrations
 
-  def after_create(record)
+  # don't check after_create, otherwise the user will have a membership already
+  # and no emails are sent
+
+  def before_create(record)
     return if MembershipObserver.disable_notifications
     AccessMailer.project_access_notification(record.user, record.project).deliver if record.user.confirmed? && !record.project.memberships.all.collect(&:user_id).include?(record.user_id)
   end
