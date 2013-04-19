ec3cc96b2524c05a10bb3adc7c6748b6a66c7d88
elarex <jeanlrx@gmail.com>
2013-04-19 12:42:54 +0200

    updated specs, fixed it so that regular users cannot add tickets in an existing sprint/feature [#809]

--- db/schema.rb
+++ db/schema.rb
@@ -143,6 +143,7 @@ ActiveRecord::Schema.define(:version => 20130222153534) do
     t.integer  "invitation_limit"
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
+    t.text     "preferences"
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
