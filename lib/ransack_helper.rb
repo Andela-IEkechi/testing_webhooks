efb2b9f1f9879335b19f5f21698bb94d54d1581c
Angus Miller <all4miller@gmai.com>
2013-04-24 16:28:45 +0200

    fixed cost default db value for 0

--- lib/ransack_helper.rb
+++ lib/ransack_helper.rb
@@ -17,14 +17,12 @@ class RansackHelper
   }.freeze
 
   def initialize(query)
-      @query        = query
-      @term         = @query.values.first if @query
-      @all_terms    = TICKET_KEYWORDS_MAP.map{ |k,v| {v => @term} }.to_a if @term
-      @search_terms = @term.scan(/(\w+):(\w+)/).map{ |k,v| {TICKET_KEYWORDS_MAP[k.to_sym] => v} } if @term
+      @search_term  = (query.values.first.blank? ? nil : query.values.first) if query
+      @mapped_terms = @search_term.scan(/(\w+):(\w+)/).map{ |k,v| {TICKET_KEYWORDS_MAP[k.to_sym] => v} } if @search_term
   end
 
   def predicates
-    return nil unless @query
+    return nil unless @search_term
 
     label_search_with_or  ||
     label_search_with_and ||
@@ -33,13 +31,25 @@ class RansackHelper
 
   private
 
+  # the default db value for cost is 0. If you search an integer field with
+  # a string e.g. "xxx" ransack just makes it 0. That kills search as tickets
+  # with default cost are returned. Unless users search for an integer
+  # we make the term for cost -1.
+  def all_terms
+    return unless @search_term
+
+    terms = TICKET_KEYWORDS_MAP.map{ |k,v| {v => @search_term} }
+    terms.map!{ |t| t.has_key?(:last_comment_cost_eq) ? {last_comment_cost_eq: -1} : t } unless @search_term.integer?
+    terms
+  end
+
+  # one ransack group with multiple conditions. the method can be
+  # either "or" / "and" and controls how condtions are where'd in sql
+  # e.g. this turns "title:wildthings title:insomersetwest"
+  # into (title like '%wildthings%' AND title like '%insomersetwest%')
+  # e.g. this turns "title:tightasses or title:bigoltities"
+  # into (title like '%tightasses%' OR title like '%bigoltities%')
   def ransack_group(method, group)
-    # one ransack group with multiple conditions. the method can be
-    # either "or" / "and" and controls how condtions are where'd in sql
-    # e.g. this turns "title:wildthings title:insomersetwest"
-    # into (title like '%wildthings%' AND title like '%insomersetwest%')
-    # e.g. this turns "title:tightasses or title:bigoltities"
-    # into (title like '%tightasses%' OR title like '%bigoltities%')
     {
       m: method,
       g: group
@@ -47,16 +57,16 @@ class RansackHelper
   end
 
   def basic_search
-    ransack_group('or', @all_terms)
+    ransack_group('or', all_terms)
   end
 
   def label_search_with_and
-    return nil unless @term.match(":")
-    ransack_group('and', @search_terms)
+    return nil unless @search_term.match(":")
+    ransack_group('and', @mapped_terms)
   end
 
   def label_search_with_or
-    return nil unless @term.match(":") && @term.match(" or ")
-    ransack_group('or', @search_terms)
+    return nil unless @search_term.match(":") && @search_term.match(" or ")
+    ransack_group('or', @mapped_terms)
   end
 end
