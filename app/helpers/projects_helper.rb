bfeac4a16733486ca0a4ba89cec9d9ae072cea18
elarex <jeanlrx@gmail.com>
2013-05-08 13:20:55 +0200

    new view forms added in to allow for transfer [#821]

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,10 @@
 module ProjectsHelper
+
+  def valid_transfer_members(project)
+    project.memberships.collect(&:user).select(&:confirmed?).reject{|u| u.id == current_user.id}
+  end
+
+  def can_transfer_project?(project)
+    valid_transfer_members(project).any?
+  end
 end
