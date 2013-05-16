279b2137be66b900685a33bf88f1a1a92676db27
elarex <jeanlrx@gmail.com>
2013-05-16 10:42:13 +0200

    force project ticket order

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,7 @@ class Project < ActiveRecord::Base
 
   belongs_to :user
   has_many :features, :dependent => :destroy, :order => :scoped_id
-  has_many :tickets, :dependent => :destroy, :include => :comments
+  has_many :tickets, :dependent => :destroy, :include => :comments, :order => 'tickets.id'
   has_many :sprints, :dependent => :destroy, :order => :scoped_id
   has_many :ticket_statuses, :dependent => :destroy
 
