cde8d13a3d6c9de48959eeafc0f418266f53dc4b
elarex <jeanlrx@gmail.com>
2013-05-20 12:38:09 +0200

    add the overviews dropdown [#834]

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :projects, :dependent => :destroy #projects we own
   has_many :tickets, :through => :projects #tickets we are assigned to
   has_many :memberships, :include => :project, :dependent => :destroy
+  has_many :overviews, :dependent => :destroy
 
   after_create :create_account
 
