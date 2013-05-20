32ea51e38adcdd6fe1b6b05e13d36aabd8b1eab8
elarex <jeanlrx@gmail.com>
2013-05-20 08:50:42 +0200

    fix validation that prevents saving comments with assets [#832]

--- app/models/comment/asset.rb
+++ app/models/comment/asset.rb
@@ -4,7 +4,9 @@ class Comment::Asset < ActiveRecord::Base
   mount_uploader  :payload, FileUploader
 
   attr_accessible :payload
-  validates :comment, :presence => true
+
+  #NOTE: DO NOT validate this, it prevents us from saving new assest on new comments (on new tickets implicitly)
+  #validates :comment_id, :presence => true
 
   def name
     payload.file.filename rescue payload
