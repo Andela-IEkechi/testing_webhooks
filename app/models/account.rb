8e9304e1c43be49d750e734a95158b2c8e516f25
Heila767 <heila@shuntyard.co.za>
2014-01-09 12:30:20 +0200

    add methods for available storage, projects and members with tests [#898]

--- app/models/account.rb
+++ app/models/account.rb
@@ -6,15 +6,15 @@ class Account < ActiveRecord::Base
   validates :plan, presence: true
 
   def current_plan
-    Plan.new(plan)
+    Plan.new(self.plan.to_s)
   end
 
   def upgrade
-    self.plan = Plan.new(self.plan).upgrade
+    self.plan = self.current_plan.upgrade.to_s
   end
 
   def downgrade
-    self.plan = Plan.new(self.plan).downgrade
+    self.plan = self.current_plan.downgrade.to_s
   end
 
   def change_to(new_plan)
@@ -22,10 +22,29 @@ class Account < ActiveRecord::Base
   end
 
   def can_downgrade?(new_plan)
-    #TODO we should check uses counts, projects, storage to see if a user can downgrade safely
     return true if current_plan.worse_than?(new_plan) #upgrades dont count, please die in a fire
-    true
+    available_members?(new_plan) && available_storage?(new_plan) && available_projects?(new_plan)
   end
 
+  def available_members?(plan = nil)
+    plan ||= current_plan
+    @used_members = Project.closedsource.find_all_by_user_id(user.id).collect{|p| p.memberships.count}.sum
+    @plan_members = plan[:members]
+    @plan_members > @used_members
+  end
+
+  def available_storage?(plan = nil)
+    plan ||= current_plan
+    @plan_storage = plan[:storage_gb]*1024**3
+    @used_storage = Project.closedsource.find_all_by_user_id(user.id).collect{|p| p.tickets.includes(:comments).collect{ |t| t.comments.includes(:assets).collect{ |c| c.assets.collect(&:filesize)} }}.flatten.sum
+    @plan_storage > @used_storage
+  end
+
+  def available_projects?(plan = nil)
+    plan ||= current_plan
+    @used_projects = Project.closedsource.find_all_by_user_id(user.id).count
+    @plan_projects = plan[:projects]
+    @plan_projects > @used_projects
+  end
 
 end
