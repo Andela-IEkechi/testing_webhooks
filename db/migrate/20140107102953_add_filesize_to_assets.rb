9bfd91fa31011e413b1a3f5bc88383dcc17c24cd
elarex <jeanlrx@gmail.com>
2014-03-03 12:10:57 +0200

    add progress output to migration [#898]

--- db/migrate/20140107102953_add_filesize_to_assets.rb
+++ db/migrate/20140107102953_add_filesize_to_assets.rb
@@ -2,9 +2,11 @@ class AddFilesizeToAssets < ActiveRecord::Migration
   def up
     add_column :comment_assets, :filesize, :integer
     Comment::Asset.reset_column_information
+    p "storing existing asset file sizes..."
     Comment::Asset.find_each do |asset|
       asset.save
     end
+    p "done."
   end
 
   def down
