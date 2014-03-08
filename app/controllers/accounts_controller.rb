cfdc9a97832e797487ca3dc062b02f96beb65e07
Heila767 <heila@shuntyard.co.za>
2014-03-08 20:30:33 +0200

    switch around alert and notice 2, add payment update [#906]

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -10,7 +10,17 @@ class AccountsController < ApplicationController
     redirect_to edit_user_account_path(@account.user)
   end
 
-  def payment_update #TODO: Edit this for account payment update: downgrade or cancellation
+  def payment_update
+    plan = Plan::PLANS.keys.each.select{|p| Plan::PLANS[p][:price_usd] == params["li_1_price"].to_i}.first.to_s
+    if @account.can_downgrade?(plan)
+      if !(plan.blank?) && @account.change_to(plan.to_s) && @account.save
+        flash[:notice] = "Downgrade request successful. You will receive an email as soon as your downgrade is completed."
+      else
+        flash[:alert] = "Downgrade request sent. However, we encountered a problem while updating your account. Please contact accounts@conductor-app.co.za for assistance"
+      end
+    else
+      flash[:alert] = "Sorry, your account can not be downgraded at this time."
+    end
     redirect_to edit_user_account_path(@account.user)
   end
 
