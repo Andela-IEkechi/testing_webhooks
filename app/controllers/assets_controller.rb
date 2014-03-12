556ba628d2a599ff0ab8f0257436ccade0b6a25d
Heila767 <heila@shuntyard.co.za>
2014-03-12 13:07:21 +0200

    Add account status to assets, not shared [#876]

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -2,10 +2,12 @@ class AssetsController < ApplicationController
   load_and_authorize_resource :project
   load_and_authorize_resource :ticket, :through => :project, :find_by => :scoped_id
   load_and_authorize_resource :asset, :through => :ticket, :class => 'Comment::Asset'
+  include AccountStatus
 
   def download
     @asset = @ticket.assets.find(params[:asset_id])
     #user send_data while we host on s3
+    @asset.payload.set_content_type #please leave alone, this is to unfuck the test for assets controller
     send_data(@asset.payload.file.read, :filename => @asset.name, :type => @asset.payload.file.content_type)
     #use send_file once the hosting is local
     #send_file(@asset.payload, :disposition => 'attachment', :url_based_filename => false)
--- spec/controllers/assets_controller_spec.rb
+++ spec/controllers/assets_controller_spec.rb
@@ -1,5 +1,78 @@
 require 'spec_helper'
+require 'shared/account_status'
 
 describe AssetsController do
+  before (:each) do
+    login_user
+    @project = create(:project, :user => @user)
+  end
 
+  describe "blocked account" do
+    before(:each) do
+      @comment = create(:comment, :user => @user)
+      @ticket = @comment.ticket
+      @comment.project = @project
+      @comment.save
+
+      @asset = create(:comment_asset, :comment_id => @comment.id)
+      filename = "#{Rails.root}/spec/data/dummy.txt"
+      @asset.payload = FileUploader.new
+      @asset.payload.store!(File.open(filename))
+      @asset.save
+
+      @other_user = create(:user)
+      @member = create(:membership, :user_id => @user.id)
+      @user.account.block!
+    end
+
+    after(:each) do
+      @asset.payload.remove!
+    end
+
+    context "owner" do
+      before(:each) do
+        @project.user_id = @user.id
+        @project.save
+      end
+      it "should return a flash message if the project is blocked" do
+        get :download, :project_id => @project, :ticket_id => @ticket, :asset_id => @asset
+        flash[:alert].should =~ /Project can not be accessed due to outstanding payment./i
+      end
+
+      it "should redirect if the project is blocked" do
+        @project.user_id = @user.id
+        get :download, :project_id => @project, :ticket_id => @ticket, :asset_id => @asset
+      end
+
+      it "should allow access if the project is not blocked" do
+        @user.account.unblock!
+        @project.user_id = @user.id
+        get :download, :project_id => @project, :ticket_id => @ticket, :asset_id => @asset
+        response.should_not be_redirect
+      end
+    end
+
+    context "member" do
+      before(:each) do
+        @project.user_id = @other_user.id
+        @project.memberships<< @member
+        @project.save
+      end
+      it "should return a flash message if the project is blocked" do      
+        get :download, :project_id => @project, :ticket_id => @ticket, :asset_id => @asset
+        flash[:notice].should =~ /Project is currently unavailable./i
+      end
+
+      it "should redirect if the project is blocked" do
+        get :download, :project_id => @project, :ticket_id => @ticket, :asset_id => @asset
+        response.should be_redirect
+      end
+
+      it "should allow access if the project is not blocked" do
+        @project.user.account.unblock!
+        get :download, :project_id => @project, :ticket_id => @ticket, :asset_id => @asset
+        response.should_not be_redirect
+      end
+    end
+  end
 end
