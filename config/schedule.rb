defbef8e72728f9e992d4ce4f96f7066414590ff
Jean le Roux <jean@shuntyard.co.za>
2015-02-18 11:35:49 +0200

    update schedule and add whenever to deploy

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,6 +20,6 @@
 # Learn more: http://github.com/javan/whenever
 
 
-every 1.day, :at => '06:00 am' do
+every 1.day, :at => '06:00 am', :environment => 'production' do
   rake "sprint:send_status_notifications"
 end
