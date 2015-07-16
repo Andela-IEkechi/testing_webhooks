68da7f8ae174aa88587c192b024f5374e7fec7c4
Jean le Roux <jean@shuntyard.co.za>
2015-07-16 21:03:30 +0200

    guard against no assets being sent in [#944]

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -31,6 +31,7 @@ class CommentsController < ApplicationController
 
   def update
     # Delete unwanted asset payloads by looking at the updated asset_ids
+    params[:comment][:asset_ids] ||= []
     @comment.assets.select{|a| !(params[:comment][:asset_ids].include? a.id.to_s)}.each(&:remove_payload!)
     #save the new assets
     @comment.save
