2445084433040f9c1d0fb7ddcb962e0767e2eaa4
Jean le Roux <jean@shuntyard.co.za>
2016-01-18 13:03:21 +0200

    guard against race condiciton with db lock, update specs [#974]

--- lib/scoped.rb
+++ lib/scoped.rb
@@ -11,8 +11,12 @@ module Scoped
     private
     def populate_scoped_id
       sequence = "#{self.class.name.downcase.pluralize}_sequence".to_sym #eg. tickets_sequence or sprints_sequence etc
-      self.project.reload.increment!(sequence) #reload forces us to correctly increment the id
-      self.scoped_id = project.send(sequence)
+      self.project.with_lock do #lock and reload forces us to correctly increment the id
+        self.project.reload
+        self.project.increment!(sequence)
+        self.scoped_id = self.project.send(sequence)
+      end
+      self.scoped_id
     end
 
   end
