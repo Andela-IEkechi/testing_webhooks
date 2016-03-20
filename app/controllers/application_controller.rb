216e9a74b82c36728fa9ce1297a9dfbf6fa8eabd
Jean le Roux <jean@shuntyard.co.za>
2016-03-20 20:24:50 +0200

    working on login auth token problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,13 @@ class ApplicationController < ActionController::Base
   #This allows us to pass these directly into redirect calls
   # e.g. redirect_to @foo, success: 'Foo was successfully created.'
   add_flash_types :success, :warning, :danger, :info
+
+  before_action :clear_flash
+
+
+  private
+
+  def clear_flash
+    flash.clear
+  end
 end
