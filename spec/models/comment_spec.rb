83f85539b5d838f2c044fbc5fb5d7ed8bb0d7ec8
Jean le Roux <jeanlrx@gmail.com>
2016-05-23 20:22:52 +0200

    remove commented specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -19,28 +19,12 @@ RSpec.describe Comment, type: :model do
     is_expected.to be_versioned
   end  
 
-  # NOTE: I've removed the gems for now, this might all need to go later on.
-  # it { should respond_to :html}
-  # describe ".html" do
-  #   it "returns parsed markdown" do
-  #     subject.message = "*bold* **italics** `escaped`"
-  #     sample_html = "<p><em>bold</em> <strong>italics</strong> <code class=\"prettyprint\">escaped</code></p>"
-  #     expect(subject.html).to eq(sample_html)
-  #   end
-
-  #   it "deals with blank messages" do
-  #     subject.message = nil
-  #     sample_html = ""
-  #     expect(subject.html).to eq(sample_html)
-  #   end
-  # end
-
-
   describe ".previous" do
     it "returns the previous comment" do
       comment = create(:comment, ticket: ticket)
       expect(comment.previous).to eq(subject)
     end
+    
     it "returns nil unless there is a previous comment" do
       expect(subject.previous).to eq(nil)
     end
