7ef65683dc5712a203e92821b2f29fd73eec457b
Ekechi Ikenna <luccy559slim@gmail.com>
2016-05-24 15:28:32 +0100

    added model specs[#981]

--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :attachment do
     comment
+    file Refile::FileDouble.new("dummy", "logo.png", content_type: "image/png")
   end
 end
--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -5,8 +5,17 @@ RSpec.describe Attachment, type: :model do
 
   it { should belong_to(:comment) }
   it { should respond_to(:file) }
+  it { should respond_to(:filename) }
+  it { should respond_to(:file_size) }
+  it { should respond_to(:file_content_type) }
 
   it "enables paper trail" do
     is_expected.to be_versioned
-  end  
+  end
+
+  describe ".filename" do
+    it "returns the file name of an attachment" do
+      expect(subject.filename).to eq(subject.file_filename)
+    end
+  end
 end
