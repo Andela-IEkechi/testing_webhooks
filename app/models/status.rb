18c87dc52f7997d7b3a70d9c09026754c8261863
Jean le Roux <jeanlrx@gmail.com>
2016-05-27 11:59:29 +0200

    small refactors and review notes [#977]

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,7 +1,6 @@
 class Status < ApplicationRecord
-  acts_as_list scope: [:position]
-
   belongs_to :project
+  acts_as_list scope: :project
 
   validates :name, presence: true, uniqueness: {scope: :project_id}
 end
